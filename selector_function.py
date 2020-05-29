beb9a7a735c2163fca1a2f7d0435ca2d4c25d412
george georgopoulos <62259101+ggeorgopoulos1998@users.noreply.github.com>
2020-05-29 19:23:46 +0300

    Add files via upload

---
+++ selector_function.py
@@ -0,0 +1,8 @@
+def select_function(people, budget, days,):
+    budget=input()
+    people=input()
+    days=input()
+    print("your budget is: %s" % budget)
+    print("number of people: %s" % people)
+    print("number od days: %s" % days)
+    print("Your selections are %s" % list(budget,days,people))
