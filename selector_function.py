a4270325adda5b7163c54f394c092ec686dc29db
PanosChristopoulos <56166480+PanosChristopoulos@users.noreply.github.com>
2020-05-31 22:57:00 +0300

    Delete selector_function.py

--- selector_function.py
+++
@@ -1,8 +0,0 @@
-def select_function(people, budget, days,):
-    budget=input()
-    people=input()
-    days=input()
-    print("your budget is: %s" % budget)
-    print("number of people: %s" % people)
-    print("number od days: %s" % days)
-    print("Your selections are %s" % list(budget,days,people))
