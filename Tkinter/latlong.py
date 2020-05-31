efb6a1f55858e3f0b702e72de7681741ce50cab7
jimkaselouris <58703191+jimkaselouris@users.noreply.github.com>
2020-05-31 22:02:58 +0300

    Add files via upload

---
+++ Tkinter/latlong.py
@@ -0,0 +1,18 @@
+import requests
+
+url = "https://devru-latitude-longitude-find-v1.p.rapidapi.com/latlon.php"
+
+def cityLatLong(city):
+    querystring = {"location":city}
+
+    headers = {
+        'x-rapidapi-host': "devru-latitude-longitude-find-v1.p.rapidapi.com",
+        'x-rapidapi-key': "df61d8e863msh47948b951144384p16523djsn4b480d122f9d"
+        }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    response_data = response.json()
+    latitude = response_data["Results"][0]["lat"]
+    longtitude = response_data["Results"][0]["lon"]
+    #print(city,"'s latitude is",latitude,"and longtitude is",longtitude)
+    return latitude,longtitude
