14ee33495c58d4308c8778c36178fbdcc5eddcdb
PanosChristopoulos <56166480+PanosChristopoulos@users.noreply.github.com>
2020-06-01 18:13:16 +0300

    Add files via upload

--- popular.py
+++ popular.py
@@ -6,15 +6,15 @@ def popularFinder(country):
     popularDestinations = ["lisbon","istanbul","kobenhavn","prague","london","dublin","beijing","london","edinburgh","milan","rome","new york","dubai","hong kong","washington","los angeles","moscow","tokyo","new delhi","sao paolo","rio de janeiro","seoul","shanghai","houston","jakarta","doha","melbourne","nairobi","kabul","cape town","johannesburg","addis ababa","kuala lumpur","buenos aires"]
     selectedDestinations = []
     results = []
-
+    destinationCities = []
     for x in range(6):
         city = random.choice(popularDestinations)
         popularDestinations.remove(city)
         selectedDestinations.append(skyScannerAirportFinderCity(city))
-    print(selectedDestinations)
+        destinationCities.append(selectedDestinations[x][1])
 
     for x in range(len(selectedDestinations)):
         results.append(getFlights(skyScannerAirportFinder(country)[0],selectedDestinations[x][0]))
-    print(results)
+    return [destinationCities,results]
 
-popularFinder("greece")
+print(popularFinder("greece"))
