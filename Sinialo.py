77fc894c539022044af3c29bbaf24253578563f9
jimkaselouris <58703191+jimkaselouris@users.noreply.github.com>
2020-06-09 20:50:38 +0300

    Add files via upload

---
+++ Sinialo.py
@@ -0,0 +1,10 @@
+import firstScreen
+import sys
+sys.path.append('Tkinter')
+from Tkinter.firstScreen import *
+
+def main():
+	firstScreen.main()
+	root.mainloop()
+if __name__=="__main__":
+    main()
